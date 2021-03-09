de3e24586874ce4442fada6c8070d1eff719d2ac
MochSuryaSyahbana <mochsuryasyahbana@gmail.com>
2021-03-09 10:32:21 +0700

    berhasil memanggil data berdasarkan id

--- controller.js
+++ controller.js
@@ -17,3 +17,17 @@ exports.tampilsemuataman = function(req,res){
         }
     });
 };
+
+//menampillkan semua data taman berdasarkan id_taman
+
+exports.tampilberdasarkanid = function(req,res){
+    let id = req.params.id;
+    connection.query('SELECT * from tb_taman where id_taman = ?',(id),
+        function(error,rows,fields){
+            if(error){
+                connection.log(error);
+            }else{
+                response.ok(rows, res)
+            }
+        });
+};
--- routers.js
+++ routers.js
@@ -9,4 +9,7 @@ module.exports = function(app){
     app.route('/tampil')
         .get(jsonku.tampilsemuataman);
 
+    app.route('/tampil/:id')
+        .get(jsonku.tampilberdasarkanid);
+
     }
